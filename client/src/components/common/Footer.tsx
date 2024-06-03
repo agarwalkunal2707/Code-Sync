5c691d4f03051b10f5f5694ff0246896165145d1
Kunal Agarwal <agarwalkunal2707@gmail.com>
2024-06-03 17:14:36 +0530

    first commit

---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
+            <span>
+                Build with ❤️ by{" "}
+                <a
+                    href="https://github.com/agarwalkunal2707"
+                    className="text-primary underline underline-offset-1"
+                >
+                    Kunal Agarwal
+                </a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
